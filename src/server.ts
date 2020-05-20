f006128ac8bd83186f6d2c1686d65804c440dc71
JFG <lapuff@yahoo.com>
2020-05-19 21:16:57 -0400

    Cleaned some comments from previous try

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,3 @@
-//import express = require('express')
-/*
-import express from 'express';
-import routes  from './api/routes';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-
-routes(app);
-app.listen(port, function () {
-  console.log('Server started on port: ' + port);
-});
-*/
-
 import App from './app';
 import Controller from './api/controller';
 
